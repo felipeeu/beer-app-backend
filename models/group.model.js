a1d579d6203021180ff0c77d9cb661756cea884b
Felipe Domingues <felipeeu@yahoo.com.br>
2019-11-20 19:17:27 -0300

    creating mongoose model
    creating two mongoose model about basic beers classification:
    first ->group model
    second -> style model

---
+++ models/group.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let GroupSchema = new Schema({
+    name: {type: String, required: true},
+});
+
+
+
+module.exports = mongoose.model('Group', GroupSchema);
---
+++ models/style.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let StyleSchema = new Schema({
+    name: {type: String, required: true},
+    abv: {type: String , required: true},
+    ibu:{type: String , required: true},
+    pair:{type: String , required: true},
+    info: {type:String,required: true}
+});
+
+
+
+module.exports = mongoose.model('Style', StyleSchema);
