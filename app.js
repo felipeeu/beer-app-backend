bc0c4a0c24858af155a1a22ce5dbe8f71a4e90f1
Felipe Domingues <felipeeu@yahoo.com.br>
2019-11-20 21:10:22 -0300

    Change port to 4000

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const koa = require('koa');
 const app = new koa();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://felipeeu:fevi2406@clusterfelipe-kdk6j.mongodb.net/test?retryWrites=true");
+mongoose.connect("mongodb+srv://felipeeu:fevi2406@clusterfelipe-kdk6j.mongodb.net/beer?retryWrites=true");
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open',()=>{console.log('connect to database')});
 
-const PORT = 3000
+const PORT = 4000
 app.listen(PORT);
 console.log(`listening in port: ${PORT}`);
