c7c7648ae8032f0da56387e263917a3e001a5f0b
Felipe Domingues <felipeeu@yahoo.com.br>
2019-11-20 21:58:18 -0300

    install packages
    koa-graphql
    koa-mount

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const koa = require('koa');
-const app = new koa();
 const mongoose = require('mongoose');
+const mount = require('koa-mount');
+const graphqlHTTP = require('koa-graphql');
+const schema = require('./schema.js')
 
 mongoose.connect("mongodb+srv://felipeeu:fevi2406@clusterfelipe-kdk6j.mongodb.net/beer?retryWrites=true");
 var db = mongoose.connection;
@@ -8,6 +10,13 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open',()=>{console.log('connect to database')});
 
-const PORT = 4000
+const app = new koa();
+app.use(mount('/graphql', graphqlHTTP({
+    schema:schema,
+    graphiql: true
+  })));
+
+
+const PORT = process.env.PORT || 4000
 app.listen(PORT);
 console.log(`listening in port: ${PORT}`);
