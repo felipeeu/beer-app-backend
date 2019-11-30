d880f6cce76e9ff5443f0adf5890417dc81e1a51
Felipe Domingues <felipeeu@yahoo.com.br>
2019-11-30 08:46:47 -0300

    solve cors problem

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const koa = require('koa');
+const cors = require('@koa/cors');
 const mongoose = require('mongoose');
 const mount = require('koa-mount');
 const graphqlHTTP = require('koa-graphql');
 const schema = require('./graphql/schema')
+
 
 mongoose.connect("mongodb+srv://felipeeu:fevi2406@clusterfelipe-kdk6j.mongodb.net/beer?retryWrites=true");
 var db = mongoose.connection;
@@ -11,6 +13,8 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open',()=>{console.log('connect to database')});
 
 const app = new koa();
+
+app.use(cors());
 app.use(mount('/graphql', graphqlHTTP({
     schema:schema,
     graphiql: true
